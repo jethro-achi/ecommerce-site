32eef6fe156f173d5de006a9138e2f1daa4d143c
Jethro Achelam <100026183+jethro-achi@users.noreply.github.com>
2023-06-29 00:03:10 +0300

    Update urls.py

--- members/urls.py
+++ members/urls.py
@@ -6,18 +6,13 @@ from django.contrib import admin
 urlpatterns = [
     path('members/', views.members, name='members'),
 ]
-#
-#urlpatterns = [
-    #path('index/', views.indexpage, name='indexpage'),
-   # path('shopitem/', views.shopitempage, name='shopitempage'),
-
 
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('index/', views.indexpage, name='indexpage'),
     path('members/', views.members, name='members'),  # Add this line
-    path('index/', views.indexpage, name='indexpage'),  # Existing URL pattern for 'indexpage'
     path('shopitem/', views.shopitempage, name='shopitempage'),  # Existing URL pattern for 'shopitempage'
     # Rest of the URL patterns
     
